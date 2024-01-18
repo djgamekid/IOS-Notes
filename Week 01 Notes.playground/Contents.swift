c3262ad4d55fc51aa56a0a7765ed0c3470914e2f
Darren Ross <78877539+djgamekid@users.noreply.github.com>
2024-01-18 10:47:36 -0600

    Tuple Notes Added and formatting

--- Week 01 Notes.playground/Contents.swift
+++ Week 01 Notes.playground/Contents.swift
@@ -38,18 +38,61 @@ print(1,2,3,4,5,6, separator: "-")
 var mobileBrand = "Apple"
 mobileBrand = "Samsung"
 print(mobileBrand)
+
 let pi = 3.14
 print(pi)
+
 //Explicit Declaration of Variable
-var age : Int = 23 //Intentional Error
-age = age * 2
-print(age)
+var age2 : Int = 23 //Intentional Error
+age2 = age2 * 2
+print(age2)
+
 var aweMessage = "This is Superb!"
 print(aweMessage)
 print("aweMessage")
+
 var course1 = "iOS"
 var course2 = "Java"
 print(course1,course2)
 print(course1,"-",course2)
+
 print(10,20,30)
 print(12.5,15.5)
+
+//01Tuples
+
+var httpError = (errorCode : 404 , errorMessage : "Page Not Found")
+print(httpError)
+print(httpError.errorCode , terminator : ": ")
+print(httpError.errorMessage )
+
+var name = ("John","Smith")
+var fName = name.0
+var lName = name.1
+print(fName , terminator : ",")
+print(lName)
+
+var origin = (x : 0 , y : 0)
+var point = origin
+print(point)
+
+let city = (name : "Maryville" , population : 11,000)
+let ( cityName ,cityPopulation ) = (city.0 , city.1)
+print(cityName)
+print(cityPopulation)
+
+let groceries = ("bread","onions")
+print(groceries.0)
+print(groceries.1)
+print(type(of: groceries))
+
+var fname = "Joe"
+var lname = "Root"
+(fname , lname) = (lname , fname)
+print("First Name is \(fname) and Last Name is \(lname)")
+
+var cricketKit = ("handGloves" ,"helmet",("bat","ball"))
+print(cricketKit.0)
+print(cricketKit.1)
+print(cricketKit.2.0)
+print(cricketKit.2.1)
