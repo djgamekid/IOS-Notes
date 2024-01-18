9b53b3eb3bd808dc20c8f4771f03507ee75187bd
Darren Ross <78877539+djgamekid@users.noreply.github.com>
2024-01-18 10:44:27 -0600

    Variables and Constant Notes

--- Week 01 Notes.playground/Contents.swift
+++ Week 01 Notes.playground/Contents.swift
@@ -32,3 +32,24 @@ print("The new pattern is")
 print(1,2,3,4,5,6, separator: "-")
 
 
+//Variables and Constants
+
+//Declaration of variable
+var mobileBrand = "Apple"
+mobileBrand = "Samsung"
+print(mobileBrand)
+let pi = 3.14
+print(pi)
+//Explicit Declaration of Variable
+var age : Int = 23 //Intentional Error
+age = age * 2
+print(age)
+var aweMessage = "This is Superb!"
+print(aweMessage)
+print("aweMessage")
+var course1 = "iOS"
+var course2 = "Java"
+print(course1,course2)
+print(course1,"-",course2)
+print(10,20,30)
+print(12.5,15.5)
